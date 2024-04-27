e1446a82e64889d77603438f1f751ae144dc35bb
sameer <sameer@sameers-MacBook-Pro.local>
2024-04-28 02:50:24 +0530

    live url added

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
 export const connection = {
-    local:"http://localhost:4000",
-    live:"https://codeeditor-backend-1ymb.onrender.com"
+    live:"http://localhost:4000",
+    local:"https://codeeditor-backend-1ymb.onrender.com"
 }
